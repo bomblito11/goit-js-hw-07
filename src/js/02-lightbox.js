e63f92c252533343775c808248adb5e86c59d183
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-03 21:44:19 +0100

    added second task

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__link" href=${image.original} >
+      <img
+        class="gallery__image"
+        src=${image.preview}
+        data-source=${image.original}
+        alt=${image.description}
+      />
+    </a>`
+  );
+});
+
+const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
